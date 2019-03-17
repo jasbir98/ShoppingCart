3e60188a8bf67d863c10f8815c0c44347b476082
jasbir98 <jasbirjas.singh98@gmail.com>
2019-03-17 22:06:26 +0530

    Output Data from MongoDB

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
+var Product = require('../models/product');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('shop/index', { title: 'Express' });
+  Product.find(function (err,docs) {
+    if(!err)
+      res.render('shop/index', { title: 'Shopping Cart', products: docs});
+    else
+      console.log(err);
+  });
+
+
 });
 
 module.exports = router;
